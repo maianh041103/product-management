7b829279c62d5a10cd8abb3a2bd87853c22e0aaa
maianh041103 <126103546+maianh041103@users.noreply.github.com>
2023-08-30 01:50:16 +0700

    Lam trang danh sach san pham ben Client 1

--- controller/client/product.controller.js
+++ controller/client/product.controller.js
@@ -1,5 +1,21 @@
-module.exports.index = (req, res) => { // Do no se noi route tu ben index.route.js nua
+const Product = require('../../models/product.model')
+
+
+
+module.exports.index = async (req, res) => { // Do nó sẽ nối route từ bên index.route.js nữa
+    const products = await Product.find({
+        status: "active",
+        deleted: false
+    });
+    // Truyền vào file là 1 object các sản phẩm muốn tìm
+
+    const newProducts = products.map(item => {
+        item.priceNew = (item.price * (100 - item.discountPercentage) / 100).toFixed(0); // Thêm 1 key cho object
+        return item;
+    })
+
     res.render('client/pages/products/index.pug', {
-        pageTitle: "Trang danh sách sản phẩm"
+        pageTitle: "Trang danh sách sản phẩm",
+        products: newProducts
     }); // mac dinh o trang index.js tro tu thu muc view
 }
