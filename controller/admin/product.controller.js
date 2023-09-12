74af48b5d2f6ec8fc4a0f5dbc36c80d0496f4415
maianh041103 <126103546+maianh041103@users.noreply.github.com>
2023-09-12 10:02:24 +0700

    Xoa mem 1 san pham

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -72,6 +72,11 @@ module.exports.changeMulti = async (req, res) => {
 //[DELETE] /admin/products/delete-item/:id
 module.exports.deleteItem = async (req, res) => {
     const id = req.params.id;
-    await Product.deleteOne({ _id: id });
+    //await Product.deleteOne({ _id: id });
+    await Product.updateOne({ _id: id }, {
+        deleted: true,
+        deleteAt: new Date()
+    }
+    );
     res.redirect('back');
 }
