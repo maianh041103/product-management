29187df22ee1cd67a1ae61bbee7e23a0898ade11
maianh041103 <nguyenmaianh041103@gmail.com>
2023-10-07 14:55:08 +0700

    Authorizantion

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,5 +1,6 @@
 const systemConfig = require('../../config/system');
 const Account = require('../../models/account.model');
+const Role = require('../../models/role.model');
 module.exports.auth = async (req, res, next) => {
   if (!req.cookies.token) {
     res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
@@ -8,11 +9,14 @@ module.exports.auth = async (req, res, next) => {
       token: req.cookies.token,
       deleted: false,
       status: "active"
-    })
+    }).select("-password");
     if (!user) {
       res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
     }
     else {
+      const roleUser = await Role.findOne({ _id: user.role_id })
+        .select("title permissions");
+      res.locals.user = roleUser;
       next();
     }
   }
