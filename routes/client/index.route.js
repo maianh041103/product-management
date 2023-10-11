324d5cb5528b3a45a198d18bbf001cfa730f31b1
maianh041103 <nguyenmaianh041103@gmail.com>
2023-10-11 09:32:20 +0700

    sub-menu chung cho client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,10 @@
 const productRoute = require("./product.route");
 const homeRoute = require("./home.route");
+const categoryMiddlerware = require("../../middlewares/client/productCategory.middlerware");
 
 module.exports = (app) => {
+    app.use(categoryMiddlerware.category);
+
     app.use('/', homeRoute);
 
     app.use('/products', productRoute);
