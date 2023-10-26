464718ccb5df06fab4b879664d24f1314bc4859c
maianh041103 <nguyenmaianh041103@gmail.com>
2023-10-26 14:26:16 +0700

    Lam trang dashboard

--- controller/admin/dashboard.controller.js
+++ controller/admin/dashboard.controller.js
@@ -1,7 +1,54 @@
+const Account = require('../../models/account.model');
+const ProductCategory = require('../../models/product-category.model');
+const Product = require('../../models/product.model');
+const User = require('../../models/user.model');
+
 // [GET] /admin/dashboard
 
-module.exports.dashboard = (req, res) => {
+module.exports.dashboard = async (req, res) => {
+    const account = await Account.findOne({ token: req.cookies.token });
+    const statistic = {
+        categoryProduct: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        },
+        product: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        },
+        account: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        },
+        user: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        }
+    };
+
+    statistic.categoryProduct.total = await ProductCategory.count({ deleted: false });
+    statistic.categoryProduct.active = await ProductCategory.count({ deleted: false, status: "active" });
+    statistic.categoryProduct.inactive = await ProductCategory.count({ deleted: false, status: "inactive" });
+
+    statistic.product.total = await Product.count({ deleted: false });
+    statistic.product.active = await Product.count({ deleted: false, status: "active" });
+    statistic.product.inactive = await Product.count({ deleted: false, status: "inactive" });
+
+    statistic.account.total = await Account.count({ deleted: false });
+    statistic.account.active = await Account.count({ deleted: false, status: "active" });
+    statistic.account.inactive = await Account.count({ deleted: false, status: "inactive" });
+
+    statistic.user.total = await User.count({ deleted: false });
+    statistic.user.active = await User.count({ deleted: false, status: "active" });
+    statistic.user.inactive = await User.count({ deleted: false, status: "inactive" });
+
     res.render('admin/pages/dashboard/index.pug', {
-        pageTitle: "Trang tổng quan"
+        pageTitle: "Trang tổng quan",
+        account: account,
+        statistic: statistic
     })
 }
--- routes/admin/dashboard.route.js
+++ routes/admin/dashboard.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const route = express.Router();
 
 const controller = require('../../controller/admin/dashboard.controller');
+
 route.get('/', controller.dashboard); // Trang chủ của trang dashboard
 
 module.exports = route;
